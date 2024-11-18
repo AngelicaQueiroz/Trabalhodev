66d446aaa7a4f881324a12ca3e9d813b19a6bd44
AngelicaQueiroz <114710543+AngelicaQueiroz@users.noreply.github.com>
2024-11-17 21:06:38 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,51 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './src/pages/login/index';
+import SignUp from './src/pages/login/signup';
+import Cardapio from './src/pages/cardapio/cardapio';
+
+// Define types for the navigation stack
+export type RootStackParamList = {
+  Login: undefined;
+  SignUp: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar style="auto" />
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen 
+          name="Login" 
+          component={Login} 
+          options={{ title: 'Login' }} 
+        />
+        <Stack.Screen 
+          name="SignUp" 
+          component={SignUp} 
+          options={{ title: 'Sign Up' }} 
+        />
+
+        <Stack.Screen 
+          name="Cardapio" 
+          component={Cardapio} 
+          options={{ title: 'Cardapio' }} 
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
